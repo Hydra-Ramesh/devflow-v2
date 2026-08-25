4f1eb71b54e29ce62dbbab6addaebf673ddd0537
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2026-08-25 21:33:18 +0530

    Add Signup.jsx

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+import SignupForm from '../components/auth/SignupForm';
+import AuthLayout from '../components/layout/AuthLayout';
+
+export default function Signup() {
+  return (
+    <AuthLayout>
+      <SignupForm />
+    </AuthLayout>
+  );
+}
