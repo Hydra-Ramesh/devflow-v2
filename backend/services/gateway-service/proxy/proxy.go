6a14ded28a2bd31341df0fd56f86594c7a2fcf18
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2026-08-28 06:33:08 +0530

    Reverse Proxy Added

---
+++ backend/services/gateway-service/proxy/proxy.go
@@ -0,0 +1,27 @@
+package proxy
+
+import (
+	"log"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Handler(targetURLStr string, pathPrefixToStrip string, pathPrefixToPrepend string) gin.HandlerFunc {
+	target, err := url.Parse(targetURLStr)
+	if err != nil {
+		log.Fatalf("Invalid target URL for proxy: %s (%v)", targetURLStr, err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	proxy.Transport = SharedTransport
+	proxy.FlushInterval = -1
+
+	proxy.Director = NewDirector(target, proxy.Director, pathPrefixToStrip, pathPrefixToPrepend)
+	proxy.ErrorHandler = NewErrorHandler(targetURLStr)
+
+	return func(c *gin.Context) {
+		proxy.ServeHTTP(c.Writer, c.Request)
+	}
+}
