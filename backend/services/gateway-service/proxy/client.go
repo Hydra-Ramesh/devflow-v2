0afc24a9f430eac159ffdc8223aa1372d1767972
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2026-08-28 06:28:14 +0530

    Client Proxy

---
+++ backend/services/gateway-service/proxy/client.go
@@ -0,0 +1,21 @@
+package proxy
+
+import (
+	"net"
+	"net/http"
+	"time"
+)
+
+var SharedTransport = &http.Transport{
+	Proxy: http.ProxyFromEnvironment,
+	DialContext: (&net.Dialer{
+		Timeout:   10 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}).DialContext,
+	ForceAttemptHTTP2:     true,
+	MaxIdleConns:          500,
+	MaxIdleConnsPerHost:   100,
+	IdleConnTimeout:       90 * time.Second,
+	TLSHandshakeTimeout:   10 * time.Second,
+	ExpectContinueTimeout: 1 * time.Second,
+}
