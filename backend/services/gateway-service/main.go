ab386c53fb2676bbac5402c800b7bdc8565bd57f
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2026-08-28 06:38:15 +0530

    feat: add main server

--- backend/services/gateway-service/main.go
+++ backend/services/gateway-service/main.go
@@ -1,1 +1,19 @@
 package gatewayservice
+
+import (
+	"github.com/Hydra-Ramesh/devflow-v2/backend/services/gateway-service/config"
+	"github.com/Hydra-Ramesh/devflow-v2/backend/services/gateway-service/log"
+	"github.com/Hydra-Ramesh/devflow-v2/backend/services/gateway-service/router"
+	"github.com/Hydra-Ramesh/devflow-v2/backend/services/gateway-service/server"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+	r := router.New(cfg)
+
+	srv := server.New(cfg, r)
+
+	if err := srv.Start(); err != nil {
+		log.Fatal("Server terminated unexpectedly: %v", err)
+	}
+}
