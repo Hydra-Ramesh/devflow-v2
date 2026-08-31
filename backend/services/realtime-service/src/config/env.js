9475d0270e364a895d4addfb63eaaeb8e5d16a60
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2026-09-01 03:55:52 +0530

    Add Env Setup File

---
+++ backend/services/realtime-service/src/config/env.js
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const PORT = process.env.PORT || 5022;
+export const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
+export const KAFKA_BROKER = (process.env.KAFKA_BROKER || 'localhost:9092').split(',');
+export const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
