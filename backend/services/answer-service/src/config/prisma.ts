050162cbfdf3e70eb7cec47868ccc8698eaf4247
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2026-09-01 20:30:26 +0530

    feat:add prisma connection

---
+++ backend/services/answer-service/src/config/prisma.ts
@@ -0,0 +1,11 @@
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+prisma.$connect().then(() => {
+  console.log('Connected to the database');
+}).catch((error: unknown) => {
+  console.error('Error connecting to the database:', error);
+});
+
+export default prisma;
