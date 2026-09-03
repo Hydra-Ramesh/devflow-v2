8215140d38a9b44b3f533a9898d08d162dcf9958
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2026-09-03 17:04:29 +0530

    add error utils

---
+++ backend/services/notification-service/src/utils/error.ts
@@ -0,0 +1,19 @@
+export class AppError extends Error {
+  statusCode: number;
+  isOperational: boolean;
+  status: string;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message: string = 'Resource not found') {
+    super(message, 404);
+  }
+}
